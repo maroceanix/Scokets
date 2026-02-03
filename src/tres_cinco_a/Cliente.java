d3f9abc41526fd163469c96e95b985b81321e6fb
maroceanix <marmoralespalop@gmail.com>
2026-02-03 14:14:20 +0100

    si

---
+++ src/tres_cinco_a/Cliente.java
@@ -0,0 +1,10 @@
+package tres_cinco_a;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tres_cinco_a/Servidor.java
@@ -0,0 +1,12 @@
+package tres_cinco_a;
+
+import java.net.DatagramPacket;
+
+public class Servidor {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
